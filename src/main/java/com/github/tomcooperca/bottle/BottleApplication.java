ba20520208b70a6d32ae9ddb7a6290e0c2d0a369
Thomas Cooper <thomas.cooper@rbc.com>
2018-06-10 10:07:33 -0400

    revert back to writing directly to repo for test data

--- src/main/java/com/github/tomcooperca/bottle/BottleApplication.java
+++ src/main/java/com/github/tomcooperca/bottle/BottleApplication.java
@@ -1,5 +1,7 @@
 package com.github.tomcooperca.bottle;
 
+import com.github.tomcooperca.bottle.message.Message;
+import com.github.tomcooperca.bottle.message.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +17,8 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BottleApplication {
@@ -28,16 +32,14 @@ public class BottleApplication {
 	@RequiredArgsConstructor
 	public static class SaveTestMessages implements CommandLineRunner {
 
-		@Value("${server.port}")
-		int port;
-
-		private RestTemplate restTemplate = new RestTemplate();
+		private final MessageRepository messageRepository;
 
 		@Override
 		public void run(String... args) throws Exception {
 			URI testMessages = ClassLoader.getSystemResource("test_messages.txt").toURI();
 			Files.lines(Paths.get(testMessages))
-					.forEach(s -> restTemplate.postForLocation("http://localhost:".concat(String.valueOf(port)).concat("/message"), s));
+					.forEach(s -> messageRepository.save(new Message(UUID.randomUUID().toString(), s,
+							"localhost", Locale.getDefault().toString())));
 		}
 	}
 
@@ -50,10 +52,7 @@ public class BottleApplication {
 		@Value("${bottle.test.url}")
 		String url;
 
-		@Value("${server.port}")
-		int port;
-
-		private RestTemplate restTemplate = new RestTemplate();
+		private final MessageRepository messageRepository;
 
 		@Override
 		public void run(String... args) throws Exception {
@@ -61,7 +60,8 @@ public class BottleApplication {
 			ResponseEntity<String> responseEntity = restTemplate.getForEntity(URI.create(url), String.class);
 			if (responseEntity.getStatusCode().is2xxSuccessful()) {
 				Arrays.asList(responseEntity.getBody().split("\\n"))
-						.forEach(s -> restTemplate.postForLocation("http://localhost:".concat(String.valueOf(port)).concat("/message"), s));
+						.forEach(s -> messageRepository.save(new Message(UUID.randomUUID().toString(), s,
+								"localhost", Locale.getDefault().toString())));
 			}
 		}
 	}
