c0c9f6edd3e75b45d77da1e47c8a1c401da887a5
Thomas Cooper <thomas.cooper@rbc.com>
2018-06-10 14:44:51 -0400

    add jacoco and failsafe plugins, use IntegrationTest for category

---
+++ src/main/java/com/github/tomcooperca/bottle/IntegrationTest.java
@@ -0,0 +1,4 @@
+package com.github.tomcooperca.bottle;
+
+public interface IntegrationTest {
+}
--- src/test/java/com/github/tomcooperca/bottle/BottleApplicationTests.java
+++ src/test/java/com/github/tomcooperca/bottle/BottleApplicationTests.java
@@ -1,12 +1,14 @@
 package com.github.tomcooperca.bottle;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Category(value = IntegrationTest.class)
 public class BottleApplicationTests {
 
 	@Test
